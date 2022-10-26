76547197369d09bfa4eaa3d29817e4c60bb5d2a7
ViktoriaFomenko <94681488+ViktoriaFomenko@users.noreply.github.com>
2022-10-26 13:58:24 +0300

    add register and login

---
+++ controllers/auth/login.js
@@ -0,0 +1,24 @@
+const { User } = require("../../models/user")
+
+const { RequestError } = require("../../helpers")
+
+const bcrypt = require("bcryptjs")
+
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw RequestError(401, "Email or password is wrong");
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+        throw RequestError(401, "Email or password is wrong");
+    }
+    const token = "1245tgd.34fsdf.4232";
+    res.json({
+        token,
+    })
+}
+
+module.exports = login;
